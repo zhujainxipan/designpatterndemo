91058c7416b9c9cab03d0c353f1b853015c6b1c3
安诺爱成长 <1399487511@qq.com>
2016-07-04 17:55:40 +0800

    简单建造者模式示例

---
+++ src/com/ht/builder/simplebuilder/Client.java
@@ -0,0 +1,13 @@
+package com.ht.builder.simplebuilder;
+
+/**
+ * Created by annuoaichengzhang on 16/7/4.
+ */
+public class Client {
+    public static void main(String[] args) {
+        Product product = new Product.Builder().buildPartA("aaaa")
+                .buildPartB("bbbb")
+                .build();
+        System.out.print(product.partA + "::" + product.partB);
+    }
+}
---
+++ src/com/ht/builder/simplebuilder/Product.java
@@ -0,0 +1,40 @@
+package com.ht.builder.simplebuilder;
+
+/**
+ * Created by annuoaichengzhang on 16/7/4.
+ */
+public class Product {
+    public String partA;
+    public String partB;
+
+    private Product(Builder builder) {
+        this.partA = builder.partA;
+        this.partB = builder.partB;
+    }
+
+
+    public static class Builder {
+        public String partA;
+        public String partB;
+
+
+        public Product.Builder buildPartA(String partA) {
+            this.partA = partA;
+            return this;
+        }
+
+
+        public Product.Builder buildPartB(String partB) {
+            this.partB = partB;
+            return this;
+        }
+
+
+        public Product build() {
+            return new Product(this);
+        }
+
+    }
+
+
+}
